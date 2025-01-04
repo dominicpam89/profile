e4b24bf006fdaeb000ed8b9c55c3d40252996905
dominicpam89 <dominicpam89@gmail.com>
2025-01-04 13:19:47 +0700

    UI: setup fonts system

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next"
-import localFont from "next/font/local"
 import "./globals.css"
+import { Poppins, Figtree } from "next/font/google"
 
-const geistSans = localFont({
-   src: "./fonts/GeistVF.woff",
-   variable: "--font-geist-sans",
-   weight: "100 900",
+const fontPrimary = Poppins({
+   subsets: ["latin"],
+   weight: ["900", "800", "700", "600"],
+   variable: "--font-primary",
 })
-const geistMono = localFont({
-   src: "./fonts/GeistMonoVF.woff",
-   variable: "--font-geist-mono",
-   weight: "100 900",
+
+const fontSecondary = Figtree({
+   subsets: ["latin"],
+   weight: ["300", "400", "500"],
+   variable: "--font-secondary",
 })
 
 export const metadata: Metadata = {
@@ -26,7 +27,7 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            className={`${fontPrimary.variable} ${fontSecondary.variable} antialiased`}
          >
             {children}
          </body>
